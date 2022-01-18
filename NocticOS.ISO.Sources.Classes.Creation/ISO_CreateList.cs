2ed357c6d836f375971147e01b93ae096e3c9ad8
VerticalMoron <ThisIsEquinoctic@gmail.com>
2022-01-19 00:23:38 +0800

    Create ISO_CreateList.cs

---
+++ NocticOS.ISO.Sources.Classes.Creation/ISO_CreateList.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Main.ISO.Program;
+
+namespace NocticOS.ISO.Sources.Classes.Creation
+{
+    public class ISO_CreateList
+    {
+        public void ExecCreateList(bool IsMissingParams)
+        {
+            
+        }
+    }
+}
