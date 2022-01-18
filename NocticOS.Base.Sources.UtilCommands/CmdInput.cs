c2fa01c41928f77f489e4de1191a19a7bded0964
VerticalMoron <ThisIsEquinoctic@gmail.com>
2022-01-18 12:05:09 +0800

    Add an optional parameter for output.

--- NocticOS.Base.Sources.UtilCommands/CmdInput.cs
+++ NocticOS.Base.Sources.UtilCommands/CmdInput.cs
@@ -8,30 +8,36 @@ namespace NocticOS.Base.Sources.UtilCommands
     public class CmdInput
     {
         Base_Mode BaseMain = new Base_Mode();
-        public void InputStr()
+        public void InputStr(bool showOutput)
         {
             var curStr = "";
             Console.Write("\nInput some text for your string: ");
             curStr = Console.ReadLine();
-            Console.WriteLine("\nString: " + curStr + "\n");
+            if (showOutput) {
+                Console.WriteLine("\nString: " + curStr + "\n");
+            }
             BaseMain.BaseMode(false);
         }
 
-        public void InputInt()
+        public void InputInt(bool showOutput)
         {
             var curInt = 0;
             Console.Write("\nInput some numbers for your integer: ");
             curInt = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nInteger: " + curInt + "\n");
+            if (showOutput) {
+                Console.WriteLine("\nInteger: " + curInt + "\n");
+            }
             BaseMain.BaseMode(false);
         }
 
-        public void InputDouble()
+        public void InputDouble(bool showOutput)
         {
             var curDouble = 0.0;
             Console.Write("\nInput some numbers for your double: ");
             curDouble = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\nDouble: " + curDouble + "\n");
+            if (showOutput) {
+                Console.WriteLine("\nDouble: " + curDouble + "\n");
+            }
             BaseMain.BaseMode(false);
         }
     }
--- NocticOS.ISO.Sources.Classes.Utility/ISO_Input.cs
+++ NocticOS.ISO.Sources.Classes.Utility/ISO_Input.cs
@@ -8,30 +8,36 @@ namespace NocticOS.ISO.Sources.Classes.Utility
     public class ISO_Input
     {
         ISO_Mode IsoMain = new ISO_Mode();
-        public void InputStr()
+        public void InputStr(bool showOutput)
         {
             var curStr = "";
             Console.Write("\nInput some text for your string: ");
             curStr = Console.ReadLine();
-            Console.WriteLine("\nString: " + curStr + "\n");
+            if (showOutput) {
+                Console.WriteLine("\nString: " + curStr + "\n");
+            }
             IsoMain.IsoMode(false);
         }
 
-        public void InputInt()
+        public void InputInt(bool showOutput)
         {
             var curInt = 0;
             Console.Write("\nInput some numbers for your integer: ");
             curInt = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nInteger: " + curInt + "\n");
+            if (showOutput) {
+                Console.WriteLine("\nInteger: " + curInt + "\n");
+            }
             IsoMain.IsoMode(false);
         }
 
-        public void InputDouble()
+        public void InputDouble(bool showOutput)
         {
             var curDouble = 0.0;
             Console.Write("\nInput some numbers for your double: ");
             curDouble = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\nDouble: " + curDouble + "\n");
+            if (showOutput) {
+                Console.WriteLine("\nDouble: " + curDouble + "\n");
+            }
             IsoMain.IsoMode(false);
         }
     }
