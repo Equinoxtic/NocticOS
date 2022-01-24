bf98ff18a43ba0e4ac50ae1ddc6cf76e53c14233
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-24 17:46:42 +0800

    Update BaseModeHelp.cs

--- Main.Sources.Help/BaseModeHelp.cs
+++ Main.Sources.Help/BaseModeHelp.cs
@@ -7,6 +7,13 @@ namespace NocticOS.Main.Sources.Help
 {
     public class BaseModeHelp
     {
-        
+        public void BaseHelp()
+        {
+            Base_Mode BaseMain = new Base_Mode();
+
+            // No code yet.
+
+            BaseMain.BaseMode(false);
+        }
     }
 }
