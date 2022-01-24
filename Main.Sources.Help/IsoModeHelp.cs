4d43798c486e1187bb3de11f2b41b2ead4a9035a
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-24 17:49:50 +0800

    Update IsoModeHelp.cs

--- Main.Sources.Help/IsoModeHelp.cs
+++ Main.Sources.Help/IsoModeHelp.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Main.ISO;
+
 namespace NocticOS.Main.Sources.Help
 {
     public class IsoModeHelp
     {
-        
+        public void IsoHelp()
+        {
+            ISO_Mode IsoMain = new ISO_Mode();
+
+            // No code yet.
+
+            IsoMain.IsoMode(false);
+        }
     }
 }
