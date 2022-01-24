ce1739748780c53e47b6bdfe919c86eee11b8802
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-24 17:38:21 +0800

    Remove `.Classes` from the directory.

--- NocticOS.ISO.Sources.Classes.Creation/ISO_MakeFile.cs
+++
@@ -1,7 +0,0 @@
-namespace NocticOS.ISO.Sources.Classes.Creation
-{
-    public class ISO_MakeFile
-    {
-        
-    }
-}
---
+++ NocticOS.ISO.Sources.Creation/ISO_MakeFile.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Main.ISO;
+
+namespace NocticOS.ISO.Sources.Classes.Creation
+{
+    public class ISO_MakeFile
+    {
+        public void MakeNewFile()
+        {
+
+        }
+    }
+}
