45f4a54c4dd1e79f5e4dc8644c1e1386102c68e7
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-24 17:46:25 +0800

    Create BaseModeHelp.cs

---
+++ Main.Sources.Help/BaseModeHelp.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Main.Base;
+
+namespace NocticOS.Main.Sources.Help
+{
+    public class BaseModeHelp
+    {
+        
+    }
+}
