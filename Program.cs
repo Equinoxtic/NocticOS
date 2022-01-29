b029d67f819f300554cb798d1693623dd9c4f234
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-29 16:46:30 +0800

    Code Rewrite.

--- Program.cs
+++ Program.cs
@@ -2,13 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Main.ISO;
-using Main.Base;
-using NocticOS.NocticUtility;
-using NocticOS.ISO.Sources.Utility;
-using NocticOS.ISO.Sources.Creation;
-using NocticOS.Base.Sources.UtilCommands;
-using NocticOS.Base.Sources.CreationCommands;
 
 namespace NocticOS
 {
@@ -16,19 +9,6 @@ namespace NocticOS
 	{
 		public static void Main(String[] args)
 		{
-			ISO_Mode IsoMain = new ISO_Mode();
-			Base_Mode baseMain = new Base_Mode();
-			NocticCmdList Cmdls = new NocticCmdList();
-			HelpFlags FlagsForHelp = new HelpFlags();
-
-			ISO_Input InputForIso = new ISO_Input();
-			ISO_Math MathForIso = new ISO_Math();
-			ISO_ObjectCreation ISOCreation = new ISO_ObjectCreation();
-
-			CmdInput BaseInput = new CmdInput();
-			CmdMath BaseMath = new CmdMath();
-			CmdObjectCreation BaseObjCreation = new CmdObjectCreation();
-
 			var curDir = @"C:\NocticOS";
 
 			string[] createDirs = {
@@ -45,6 +25,7 @@ namespace NocticOS
 					System.IO.Directory.CreateDirectory(createDirs[i]);
 				}
 			}
+
 			var curSelected = "";
 			
 			Console.WriteLine("\nNocticOS - Version (1.1.4)");
@@ -54,24 +35,6 @@ namespace NocticOS
 
 			switch (curSelected)
 			{
-				// Help Commands
-				case "help" : Cmdls.OpenCmdList(); break;
-				case "help -flags" : FlagsForHelp.DisplayFlags(); break;
-				case "help iso" : break;
-
-				// Help ISO
-				case "help -i input" : InputForIso.InputLs(true, false, false, false); break;
-				case "help -i math" : MathForIso.MathLs(true, false, false,false); break;
-				case "help -i create" : ISOCreation.CreationList(true, false, false, false); break;
-
-				// Help Base
-				case "help -b input" : BaseInput.InputLs(true, false, false, false); break;
-				case "help -b math" : BaseMath.MathLs(true, false, false, false); break;
-				case "help -b create" : BaseObjCreation.CreationList(true, false, false, false); break;
-
-				// Main Commands
-				case "iso" : IsoMain.IsoMode(true); break;
-				case "base" : baseMain.BaseMode(true); break;
 				case "exit" : Console.WriteLine("Exiting program..."); Thread.Sleep(3000); break;
 			}
 		}
--- ProgramReturn.cs
+++ ProgramReturn.cs
@@ -1,13 +1,6 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Main.ISO;
-using Main.Base;
-using NocticOS.ISO.Sources.Utility;
-using NocticOS.ISO.Sources.Creation;
-using NocticOS.Base.Sources.UtilCommands;
-using NocticOS.Base.Sources.CreationCommands;
-using NocticOS.NocticUtility;
 
 namespace NocticOS
 {
@@ -15,19 +8,6 @@ namespace NocticOS
     {
         public void DoReturn(bool showInfo)
 		{
-			ISO_Mode IsoMain = new ISO_Mode();
-			Base_Mode baseMain = new Base_Mode();
-			NocticCmdList Cmdls = new NocticCmdList();
-			HelpFlags FlagsForHelp = new HelpFlags();
-
-			ISO_Input InputForIso = new ISO_Input();
-			ISO_Math MathForIso = new ISO_Math();
-			ISO_ObjectCreation ISOObjCreation = new ISO_ObjectCreation();
-
-			CmdInput BaseInput = new CmdInput();
-			CmdMath BaseMath = new CmdMath();
-			CmdObjectCreation BaseObjCreation = new CmdObjectCreation();
-
 			var curSelected = "";
 
 			if (showInfo) {
@@ -40,24 +20,6 @@ namespace NocticOS
 
 			switch (curSelected)
 			{
-				// Help Commands
-				case "help" : Cmdls.OpenCmdList(); break;
-				case "help -flags" : FlagsForHelp.DisplayFlags(); break;
-				case "help iso" : break;
-
-				// ISO Commands
-				case "help -i input" : InputForIso.InputLs(true, false, false, false); break;
-				case "help -i math" : MathForIso.MathLs(true, false, false, false); break;
-				case "help -i create" : ISOObjCreation.CreationList(true, false, false, false); break;
-				
-				// Base Commands
-				case "help -b input" : BaseInput.InputLs(true, false, false, false); break;
-				case "help -b math" : BaseMath.MathLs(true, false, false, false); break;
-				case "help -b create" : BaseObjCreation.CreationList(true, false, false, false); break;
-
-				// Main Commands
-				case "iso" : IsoMain.IsoMode(true); break;
-				case "base" : baseMain.BaseMode(true); break;
 				case "exit" : Console.WriteLine("Exiting program..."); Thread.Sleep(3000); break;
 			}
 		}
