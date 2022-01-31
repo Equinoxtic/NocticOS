db3c6b674c7a893bac893d70c6dcedc43ec17247
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-31 11:00:13 +0800

    Add `DirectoryBuilder.cs`

---
+++ NocticOS.Main/DirectoryBuilder.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+namespace NocticOS.Main
+{
+    public class DirectoryBuilder
+    {
+        public void DirBuild()
+        {
+            ProgramReturn ReturnToProgram = new ProgramReturn();
+
+            var curDir = @"C:\NocticOS";
+
+			string[] createDirs = {
+				@"C:\NocticOS\Objects",
+				@"C:\NocticOS\Functions",
+				@"C:\NocticOS\Classes",
+				@"C:\NocticOS\Data"
+			};
+
+            System.IO.Directory.CreateDirectory(curDir);
+			
+		    if (System.IO.Directory.Exists(curDir)) {
+				for (int i = 0; i < 3; i++) {
+					System.IO.Directory.CreateDirectory(createDirs[i]);
+				}
+			}
+
+            ReturnToProgram.DoReturn(false);
+        }
+    }
+}
