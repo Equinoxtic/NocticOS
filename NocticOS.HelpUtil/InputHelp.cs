a023c630905899ba7e08dcf37b791c1479f52e6c
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-01 20:17:34 +0800

    Create InputHelp.cs

---
+++ NocticOS.HelpUtil/InputHelp.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.HelpUtil
+{
+    public class InputHelp
+    {
+        public void InputHelpState(bool isMissingParams)
+        {
+            // No code yet.
+        }
+    }
+}
