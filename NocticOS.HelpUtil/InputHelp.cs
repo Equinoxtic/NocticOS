949e0e74114e1e5d619890b27896f2047c6e4646
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-01 20:20:36 +0800

    Update InputHelp.cs

--- NocticOS.HelpUtil/InputHelp.cs
+++ NocticOS.HelpUtil/InputHelp.cs
@@ -8,7 +8,17 @@ namespace NocticOS.HelpUtil
     {
         public void InputHelpState(bool isMissingParams)
         {
-            // No code yet.
+            string[] InputCmdLs = {};
+
+            string[] InputCmdDescs = {};
+
+            if (isMissingParams) {
+                Console.Write("\nMissing parameters, please enter the following:\n");
+            }
+
+            for (int i = 0; i < null; i++) {
+                
+            }
         }
     }
 }
