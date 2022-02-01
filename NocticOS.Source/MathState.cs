08b99aaea8fe828784d37b65c32f71ba47e5e5ed
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-01 18:45:46 +0800

    Update MathState.cs

--- NocticOS.Source/MathState.cs
+++ NocticOS.Source/MathState.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using NocticOS.Utility;
 
 namespace NocticOS.Source
 {
@@ -9,15 +10,33 @@ namespace NocticOS.Source
         public void OpenState()
         {
             ProgramReturn Program = new ProgramReturn();
+            StandardMath standardMath = new StandardMath();
 
+            var operOpts = "";
+            
             string[] OperList = {
-                "[add] Addition |",
-                "[subtract] Subtraction |",
-                "[multiply] Multiplication |",
+                "[add] Addition",
+                "[subtract] Subtraction",
+                "[multiply] Multiplication",
                 "[divide] Division\n"
             };
 
-            Program.DoReturn(false);
+            Console.WriteLine("\nSelect an Operation:\n");
+
+            for (int i = 0; i < 4; i++) {
+                Console.Write(OperList[i] + "\n");
+            }
+
+            Console.Write("~ ");
+            operOpts = Console.ReadLine();
+
+            switch(operOpts)
+            {
+                case "add" : standardMath.NocticMath("add", true); break;
+                case "subtract" : standardMath.NocticMath("subtract", true); break;
+                case "multiply" : standardMath.NocticMath("multiply", true); break;
+                case "divide" : standardMath.NocticMath("divide", true); break;
+            }
         }
     }
 }
