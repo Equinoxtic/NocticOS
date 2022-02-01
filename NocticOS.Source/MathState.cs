72eb7f97b4197f93e9370b2344de418846213b9e
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-01 16:35:37 +0800

    New C# class: `MathState.cs`

---
+++ NocticOS.Source/MathState.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Source
+{
+    public class MathState
+    {
+        public void OpenState()
+        {
+            // No code yet.
+        }
+    }
+}
