aaa9e55dee35b8c91fb288bf81668902b5347974
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-01 18:47:15 +0800

    Update `Program` and `ProgramReturn.cs`

--- ProgramReturn.cs
+++ ProgramReturn.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using NocticOS.Main;
 using NocticOS.Source;
 using NocticOS.HelpUtil;
+using NocticOS.Utility;
 
 namespace NocticOS
 {
@@ -12,9 +13,16 @@ namespace NocticOS
         public void DoReturn(bool showInfo)
 		{
 			DirectoryBuilder DirBuilder = new DirectoryBuilder();
+
+			// Help Utils
 			HelpState helpState = new HelpState();
+
+			// Utils
+			StandardMath standardMath = new StandardMath();
+
+			// States /  Classes
 			InputState StandardInput = new InputState();
-			MathState StandardMath = new MathState();
+			MathState mathState = new MathState();
 
 			var curSelected = "";
 
@@ -29,10 +37,18 @@ namespace NocticOS
 			switch (curSelected)
 			{
 				case "help" : helpState.OpenState(); break;
+
 				case "input" :  StandardInput.InputSelect(); break;
-				case "math" : StandardMath.OpenState(); break;
+
+				case "math" : mathState.OpenState(); break;
+				case "math -add" : standardMath.NocticMath("add", true); break;
+				case "math -subtract" : standardMath.NocticMath("subtract", true); break;
+				case "math -multiply" : standardMath.NocticMath("multiply", true); break;
+				case "math -divide" : standardMath.NocticMath("divide", true); break;
+
 				case "build-dirs": DirBuilder.DirBuild(); break;
-				case "exit" : Console.WriteLine("Exiting program..."); Thread.Sleep(3000); break;
+
+				case "exit" : Console.WriteLine("Exiting program...\n"); Thread.Sleep(3000); break;
 			}
 		}
     }
