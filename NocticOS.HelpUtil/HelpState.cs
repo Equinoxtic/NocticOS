b78f391139b655ffe8596522ffa70a6c438b98b0
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-02 00:57:24 +0800

    Update HelpState.cs

--- NocticOS.HelpUtil/HelpState.cs
+++ NocticOS.HelpUtil/HelpState.cs
@@ -6,27 +6,60 @@ namespace NocticOS.HelpUtil
 {
     public class HelpState
     {
+        ProgramReturn Program = new ProgramReturn();
+
+        string[] CmdLs = {
+                "help",
+                "input",
+                "math"
+            };
+
+        string[] CmdRequires = {
+            " <command>",
+            " <type>",
+            " <operation>"
+        };
+
+        string[] CmdDescs = {
+            " - Shows information and help on a command.",
+            " - Standard Input for strings, integers and doubles.",
+            " - Math for NocticOS. Does simple operations such as Addition, Subtraction, Multiplication and Division."
+        };
         public void OpenState()
         {
-            ProgramReturn Program = new ProgramReturn();
-
-            string[] CmdLs = {
-                "input"
-            };
-
-            string[] CmdRequires = {
-                " <type>"
-            };
-
-            string[] CmdDescs = {
-                " - Standard Input for strings, integers and doubles."
-            };
-
             Console.Write("\n");
 
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < 3; i++) {
                 Console.Write(CmdLs[i]+CmdRequires[i]+CmdDescs[i]+"\n");
             }
+
+            Program.DoReturn(false);
+        }
+
+        public void HelpSwitch(string curCmd)
+        {
+            Console.Write("\n");
+
+            switch(curCmd)
+            {       
+                case "input":
+                    Console.Write(
+                        CmdLs[1] + 
+                        CmdRequires[1] + 
+                        CmdDescs[1]
+                    );
+                    break;
+
+                case "math":
+                    Console.Write(
+                        CmdLs[2] + 
+                        CmdRequires[2] + 
+                        CmdDescs[2]
+                    );
+                    break;
+            }
+
+            Console.Write("\n");
 
             Program.DoReturn(false);
         }
