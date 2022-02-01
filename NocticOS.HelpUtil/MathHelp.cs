774ff008b0c539336f3c8fed45fffdb8811cf95a
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-01 19:56:11 +0800

    Create MathHelp.cs

---
+++ NocticOS.HelpUtil/MathHelp.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.HelpUtil
+{
+    public class MathHelp
+    {
+        public void OpenHelp()
+        {
+            // No code yet.
+        }
+    }
+}
