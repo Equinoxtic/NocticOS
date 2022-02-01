ead1aad8ce40d493581abbb3b613d8fb160243be
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-01 18:46:09 +0800

    Add `includeInput` parameter.

--- NocticOS.Utility/StandardMath.cs
+++ NocticOS.Utility/StandardMath.cs
@@ -6,15 +6,31 @@ namespace NocticOS.Utility
 {
     public class StandardMath
     {
-        public void NocticMath(string oper, int a, int b)
-        {   
+        public void NocticMath(string oper, bool includeInput, int a = 0, int b = 0)
+        {
+            ProgramReturn Program = new ProgramReturn();
+
+            if (includeInput)
+            {
+                Console.Write("\nEnter your first number: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter your second number: ");
+                b = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Result: ");
+
             switch(oper)
             {
-                case "add": Console.WriteLine(a+b); break;
-                case "subtract": Console.WriteLine(a-b); break;
-                case "multiply": Console.WriteLine(a*b); break;
-                case "divide" : Console.WriteLine(a/b); break;
+                case "add" : Console.Write(a+b); break;
+                case "subtract" : Console.Write(a-b); break;
+                case "multiply" : Console.Write(a*b); break;
+                case "divide" : Console.Write(a/b); break;
             }
+
+            Console.Write("\n");
+            
+            Program.DoReturn(false);
         }
     }
 }
