774965f7d4828644405e150f2df55bf55d1ba883
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-03 01:20:26 +0800

    Add C# class: `DesktopEnviromentBuilder.cs`

--- NocticOS.Main/DesktopEnviromentBuilder.cs
+++ NocticOS.Main/DesktopEnviromentBuilder.cs
@@ -4,6 +4,13 @@ namespace NocticOS.Main
 {
     public class DesktopEnviromentBuilder
     {
-        
+        public void DEBuild()
+        {
+            ProgramReturn Program = new ProgramReturn();
+
+            Console.Write("\nThis feature is still a WIP.");
+
+            Program.DoReturn(false);
+        }
     }
 }
