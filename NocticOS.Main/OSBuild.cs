3528308ab77484d481289e761ae0c623cb12fa35
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-02 09:42:15 +0800

    Add C# class: `OSBuild.cs`

---
+++ NocticOS.Main/OSBuild.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Main
+{
+    public class OSBuild
+    {
+        public void BuildOS()
+        {
+            ProgramReturn Program = new ProgramReturn();
+
+            Program.DoReturn(false);
+        }
+    }
+}
