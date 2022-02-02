d28a416690befbf85c9bc698691d8e96a07be725
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-03 01:18:05 +0800

    Add C# class: `OSBuilder.cs`

---
+++ NocticOS.Main/OSBuilder.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace NocticOS.Main
+{
+    public class OSBuilder
+    {
+        public void BuildOS()
+        {
+            ProgramReturn Program = new ProgramReturn();
+
+            Console.Write("\nThis feature is still WIP.\n");
+
+            Program.DoReturn(false);
+        }
+    }
+}
