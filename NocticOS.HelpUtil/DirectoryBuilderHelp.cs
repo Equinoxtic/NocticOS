e5ff0cb672b56d5dbddc3016e4b8fc823d579ee1
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-02 11:46:06 +0800

    Create DirectoryBuilderHelp.cs

---
+++ NocticOS.HelpUtil/DirectoryBuilderHelp.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.HelpUtil
+{
+    public class DirectoryBuilderHelp
+    {
+        public void OpenHelp(bool isMissingParams)
+        {
+            // No code yet.
+        }
+    }
+}
