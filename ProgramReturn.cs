4897d551c95b4e2285743fa4fec57f0bbb99e4ad
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-03 09:55:06 +0800

    Add `help build` command.

--- ProgramReturn.cs
+++ ProgramReturn.cs
@@ -43,6 +43,7 @@ namespace NocticOS
 				case "help" : helpState.OpenState(); break;
 				case "help input" : helpState.HelpSwitch("input"); break;
 				case "help math" : helpState.HelpSwitch("math"); break;
+				case "help build" : helpState.HelpSwitch("build"); break;
 
 				case "input" : inputHelp.OpenHelp(true); break;
 				case "input -help" : inputHelp.OpenHelp(false); break;
