7b20647ae71172d5ecaffc132e09ab186981f610
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-04 19:09:34 +0800

    Add new C# class: `CreationHelp.cs`

---
+++ NocticOS.HelpUtil/CreationHelp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NocticOS.HelpUtil
+{
+    public class CreationHelp
+    {
+        public void OpenHelp(bool isMissingParams)
+        {
+            // No code yet.
+        }
+    }
+}
