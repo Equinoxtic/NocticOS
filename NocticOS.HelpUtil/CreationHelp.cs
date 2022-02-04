3bd217cf00dd440a4833a36bb4016aaa56b18b96
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-04 19:49:29 +0800

    Add `ProgramReturn` implementation.

--- NocticOS.HelpUtil/CreationHelp.cs
+++ NocticOS.HelpUtil/CreationHelp.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NocticOS.HelpUtil
 {
@@ -6,7 +8,9 @@ namespace NocticOS.HelpUtil
     {
         public void OpenHelp(bool isMissingParams)
         {
-            // No code yet.
+            ProgramReturn Program = new ProgramReturn();
+        
+            Program.DoReturn(false);
         }
     }
 }
