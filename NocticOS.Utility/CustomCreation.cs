d0091f4c1ca88ba5a7590716b0040fa8afce2d12
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-04 22:13:31 +0800

    Implement name and type input for method: `ObjectCreation`

--- NocticOS.Utility/CustomCreation.cs
+++ NocticOS.Utility/CustomCreation.cs
@@ -10,7 +10,23 @@ namespace NocticOS.Utility
         {
             ProgramReturn Program = new ProgramReturn();
 
-            Console.Write("\nThis feature is still in WIP.\n");
+            string[] objTypes = {};
+
+            var OBJECT_NAME = "";
+            var OBJECT_TYPE = "";
+
+            Console.Write("\nInput a name for your object: ");
+            OBJECT_NAME = Console.ReadLine();
+
+            Console.Write("\nSelect an Object Type:\n");
+
+            for (int i = 0; i < 3; i++) {
+                Console.Write(objTypes[i] + "\n");
+            }
+
+            OBJECT_TYPE = Console.ReadLine();
+
+            switch(OBJECT_TYPE) {}
 
             Program.DoReturn(false);
         }
