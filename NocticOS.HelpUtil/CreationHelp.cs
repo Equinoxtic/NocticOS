c868af8cd1cd3c7c89321d5a3ce8c46e0fdff4e6
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-04 20:07:28 +0800

    Flag descriptions implementation.

--- NocticOS.HelpUtil/CreationHelp.cs
+++ NocticOS.HelpUtil/CreationHelp.cs
@@ -17,7 +17,12 @@ namespace NocticOS.HelpUtil
                 "-class"
             };
 
-            string[] creationDescs = {};
+            string[] creationDescs = {
+                " - Standard Creation selection, gives you the options for creating a custom object, function and class.",
+                " - Create a Custom Object, input it's own name and type.",
+                " - Create a Custom Function, input it's own name and allows you to select a code preset to run.",
+                " - Create a Custom Class, input it's own name and it also allows you to store objects within it.",
+            };
 
             for (int i = 0; i < null; i++) {
                 Console.Write("create ");
