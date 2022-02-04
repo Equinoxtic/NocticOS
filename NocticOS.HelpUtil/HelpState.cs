164ffee58eaba1b1dffa0ea40ae40ab5e88c3d4b
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-04 23:57:55 +0800

    Implement help for `create`

--- NocticOS.HelpUtil/HelpState.cs
+++ NocticOS.HelpUtil/HelpState.cs
@@ -14,27 +14,30 @@ namespace NocticOS.HelpUtil
             "help",
             "build",
             "input",
-            "math"
+            "math",
+            "create"
         };
 
         string[] CmdRequires = {
             " <command>",
             " <flag>",
             " <type>",
-            " <operation>"
+            " <operation>",
+            " <type>"
         };
 
         string[] CmdDescs = {
             " - Shows information and help on a command.",
             " - Builds the NocticOS files and directories.",
             " - Standard Input for strings, integers and doubles.",
-            " - Math for NocticOS. Does simple operations such as Addition, Subtraction, Multiplication and Division."
+            " - Math for NocticOS. Does simple operations such as Addition, Subtraction, Multiplication and Division.",
+            " - Allows you to create Custom Objects, Functions, and Classes!"
         };
         public void OpenState()
         {
             Console.Write("\n");
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 5; i++) {
                 Console.Write(CmdLs[i]+CmdRequires[i]+CmdDescs[i]+"\n");
             }
 
