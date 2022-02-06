d1172b30a38abc205dc3f778727fcf3c78763f1c
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-07 02:51:50 +0800

    Update `PackageInstaller.cpp`

--- NocticOS.Main/PackageInstaller.cpp
+++ NocticOS.Main/PackageInstaller.cpp
@@ -13,32 +13,6 @@ void UninstallPackage(int cur_package = 0);
 
 int main()
 {
-	string options;
-
-	string daOptions[3] = {};
-
-	cout << "Select an Option:\n";
-
-	for (int i = 0; i < 3; i++) {
-		cout << daOptions[i];
-	}
-
-	cin >> options;
-
-	switch(options)
-	{
-		case 1:
-			PackageSelection();
-			break;
-
-		case 2:
-			InstallPackage();
-			break;
-		
-		case 3:
-			UninstallPackage();
-			break;
-	}
-
+	PackageSelection();
 	return 0;
 }
