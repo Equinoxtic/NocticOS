2a22a5542a9aa3a51669b7e47379dc81828d1483
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-07 02:31:11 +0800

    Add C++ file for Package Installation.

---
+++ NocticOS.Main/PackageInstaller.cpp
@@ -0,0 +1,16 @@
+/*
+This (C++) file is used for Package Installation
+Read, "COMPILING.md" to learn how to compile this (C++) file
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void InstallPackage(int cur_package = 0);
+void UninstallPackage(int cur_package = 0);
+
+int main()
+{
+	return 0;
+}
