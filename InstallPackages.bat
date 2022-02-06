7eafcab5475bd4410f1892213efba43f0c0c1f89
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-07 01:18:24 +0800

    Implement installation prompt.

--- InstallPackages.bat
+++ InstallPackages.bat
@@ -1,1 +1,15 @@
 :: Batch Script for installing pacakges for NocticOS
+
+@ECHO OFF
+
+
+:: Install Prompt
+:INSTALLNEWTONPROMPT
+
+SET /P OPTS=Install Packages for NocticOS? [y/n]:
+
+IF %OPTS%=="y" GOTO INSTALLNEWTON
+
+IF %OPTS%=="n" ECHO	 Operation Cancelled.
+
+EXIT
