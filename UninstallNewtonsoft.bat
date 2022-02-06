19e463149bf78110f51779d52251dc6e0a1be599
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-07 01:23:05 +0800

    Uninstall Implementation.

---
+++ UninstallNewtonsoft.bat
@@ -0,0 +1,27 @@
+:: Batch Script for uninstalling Newtonsoft for NocticOS
+
+@ECHO OFF
+
+
+:: Install Prompt
+:UNINSTALLNEWTONPROMPT
+
+SET /P OPTS=Uninstall Packages for NocticOS? [y/n]: 
+
+IF %OPTS%=="y" GOTO UNINSTALLNEWTON
+
+IF %OPTS%=="n" ECHO	 Operation Cancelled.
+
+
+:: Installation
+:UNINSTALLNEWTON
+
+ECHO:
+
+ECHO Removing Newtonsoft.Json... 
+
+ECHO: 
+
+dotnet remove package Newtonsoft.Json
+
+PAUSE
