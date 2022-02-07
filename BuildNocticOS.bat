cfee62f3e372286d72046e4fcdda9a39dbbe0b28
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-07 20:45:01 +0800

    Update `BuildNocticOS.bat`

--- BuildNocticOS.bat
+++ BuildNocticOS.bat
@@ -1,2 +1,19 @@
 :: Batch Script for building NocticOS.
 
+@ECHO OFF
+
+GOTO BUILDPROMPT
+
+:BUILDPROMPT
+
+SET /P PROMPT_MSG=Would you like to build NocticOS? [y/n]: 
+
+IF %PROMPT_MSG%==y (
+	ECHO:
+	dotnet build
+	PAUSE
+) ELSE IF %PROMPT_MSG%==n (
+	ECHO:
+	ECHO Operation Cancelled.
+	PAUSE
+)
