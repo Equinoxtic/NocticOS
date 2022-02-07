9594bf733c66cc61e7fe7ed16712baff6192e42f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-07 17:59:55 +0800

    Implement action list.

--- NocticOS.Main/PkgInstaller.cpp
+++ NocticOS.Main/PkgInstaller.cpp
@@ -5,6 +5,17 @@ using namespace std;
 int main()
 {
 	int action;
+	
+	string actionsLs[2] = {
+		"[1] Install Packages",
+		"[2] Uninstall Packages"
+	};
+
+	for (int i = 0; i < 2; i++) {
+		cout << actionsLs[i];
+	}
+
+	cout << "\n";
 
 	cin >> action;
 
