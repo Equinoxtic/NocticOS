23858241c44b0a4e89852d00f6a052d28eb4afc9
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-10 00:57:48 +0800

    Create ThrowError.cs

---
+++ NocticOS.Utility/ThrowError.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Utility
+{
+    public class ThrowError
+    {
+        /*
+        * ev - Empty Value
+        * oc - Operation Cancelled
+        * mp - Missing Parameters
+        */
+    }
+}
