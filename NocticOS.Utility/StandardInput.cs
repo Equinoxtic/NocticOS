d53c179794dd60f96b74b06d6213b83d60252e32
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-10 01:58:44 +0800

    Implement empty value support for string input.

--- NocticOS.Utility/StandardInput.cs
+++ NocticOS.Utility/StandardInput.cs
@@ -7,74 +7,71 @@ namespace NocticOS.Utility
     public class StandardInput
     {
         ProgramReturn Program = new ProgramReturn();
+        ThrowError throwError = new ThrowError();
         public void InputStr(bool ShowOutput, bool ReturnToProgram)
         {
-            var curString = "";
+            string? curString;
 
             Console.Write("\nInput some text for your string: ");
             curString = Console.ReadLine();
 
-            if (ShowOutput)
-            {
-                Console.WriteLine("String: " + curString);
+            if (ShowOutput) {
+                if (String.IsNullOrEmpty(curString)) {
+                    throwError.EmptyValThrow(true);
+                } else {
+                    Console.WriteLine("String: " + curString);
+                }
             }
 
-            if (ReturnToProgram)
-            {
+            if (ReturnToProgram) {
                 Program.DoReturn(false);
             }
         }
 
         public void InputInt(bool ShowOutput, bool ReturnToProgram)
         {
-            int curInteger = 0;
+            int curInteger;
 
             Console.Write("\nInput some numbers for your integer: ");
             curInteger = Convert.ToInt32(Console.ReadLine());
 
-            if (ShowOutput)
-            {
-                Console.WriteLine("Integer: " + curInteger);
+            if (ShowOutput) {
+                    Console.WriteLine("Integer: " + curInteger);
             }
 
-            if (ReturnToProgram)
-            {
+            if (ReturnToProgram) {
                 Program.DoReturn(false);
             }
         }
 
         public void InputFloat(bool ShowOutput, bool ReturnToProgram)
         {
-            float curFloat = 0;
+            float curFloat;
 
             Console.Write("\nInput some numbers for your float: ");
             curFloat = float.Parse(Console.ReadLine());
 
-            if (ShowOutput)
-            {
+            if (ShowOutput) {
                 Console.WriteLine("Float: " + curFloat);
             }
 
-            if (ReturnToProgram)
-            {
+            if (ReturnToProgram) {
                 Program.DoReturn(false);
             }
         }
 
         public void InputDouble(bool ShowOutput, bool ReturnToProgram)
         {
-            double curDouble = 0.0;
+            double curDouble;
 
             Console.Write("\nInput some numbers for your double: ");
             curDouble = Convert.ToDouble(Console.ReadLine());
 
-            if (ShowOutput)
-            {
+            if (ShowOutput) {
                 Console.WriteLine("Double: " + curDouble);
             }
 
-            if (ReturnToProgram)
-            {
+            if (ReturnToProgram) {
                 Program.DoReturn(false);
             }
         }
