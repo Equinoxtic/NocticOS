f5d592280dda558cb738aab73980c48e4eaec54f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-18 23:37:24 +0800

    Create loopthrows.h

---
+++ NocticOS.Applications/V-Script/include/loop_build_headers/loopthrows.h
@@ -0,0 +1,15 @@
+// loopthrows.h
+
+#include<iostream>
+
+using namespace std;
+
+void EmptyValueThrow() {
+	cout << "\nEntered Empty Value.\n";
+	system("pause");
+}
+
+void PauseThrow() {
+	cout << "\n";
+	system("pause");
+}
