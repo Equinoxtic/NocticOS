deb8dd947013a3bf4b6def44de4122da2945cd6a
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-18 23:19:12 +0800

    Update loopbuild.h

--- NocticOS.Applications/V-Script/include/loopbuild.h
+++ NocticOS.Applications/V-Script/include/loopbuild.h
@@ -1,2 +1,34 @@
 // loopbuild.cpp
 
+#include "loop_build_headers/whilebuild.h"
+#include<iostream>
+
+using namespace std;
+
+void LoopBuilder()
+{
+	int curOpt;
+
+	string options[2] = {
+		"[1] For",
+		"[2] While"
+	};
+	
+	cout << "=== V-Script Loop Builder ===\n"
+
+	cout << "Select an action:\n";
+
+	for (int i = 0; i < 2; i++) {
+		cout << options[i] + "\n";
+	}
+
+	cin >> curOpt;
+
+	switch(curOpt)
+	{
+		case 1:
+		case 2:
+			CinWhile();
+			break;
+	}
+}
