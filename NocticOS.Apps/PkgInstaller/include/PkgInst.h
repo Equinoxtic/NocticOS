f0087fb00a277058d3d333fc96b1bea7496fec5f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-18 17:49:55 +0800

    Create PkgInst.h

---
+++ NocticOS.Apps/PkgInstaller/include/PkgInst.h
@@ -0,0 +1,19 @@
+// pkginst.h
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void InstallPackage()
+{
+	string curRepo = "";
+	cout << "Enter a Repository link to install: ";
+	cin >> curRepo;
+	string str = "git ";
+	str = str + "clone " + curRepo;
+	const char* command = str.c_str();
+	system(command);
+	cout << "\n\nInstallation Finished\n";
+	system("pause");
+}
