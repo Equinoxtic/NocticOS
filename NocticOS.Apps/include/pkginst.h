8dc960f35b02f647219c7acacd0992458f138fe6
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-18 16:19:46 +0800

    Update pkginst.h

--- NocticOS.Apps/include/pkginst.h
+++ NocticOS.Apps/include/pkginst.h
@@ -1,9 +1,19 @@
 // pkginst.h
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<iostream>
 
-void InstallPkg()
+using namespace std;
+
+void InstallPackage(string curRepoLink)
 {
+	/*
+	Check if repo link is null
+	*/
+	if (curRepoLink != NULL) {
+		system("git clone " + curRepoLink);
+		return 0;
+	} else {
+		cout << "Failed to install, please try again.";
+		return 1;
+	}
 }
