5e237510f6ee731ad4c18f4f9db1e44ff81bf23a
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-18 23:23:37 +0800

    Update forbuild.h

--- NocticOS.Applications/V-Script/include/loop_build_headers/forbuild.h
+++ NocticOS.Applications/V-Script/include/loop_build_headers/forbuild.h
@@ -1,1 +1,14 @@
 // forbuild.h
+
+#include "throwutility.h"
+#include<iostream>
+
+using namespace std;
+
+void ForInit(int ivar, int a, string outputStr)
+{
+	for (int i = ivar; i < a; i++) {
+		cout << outputStr;
+	}
+	ThrowPause();
+}
