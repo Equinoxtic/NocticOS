88af80e1fc556fd04b30a80fcb39e9351ee5cc66
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-18 16:31:13 +0800

    Update pkginstaller.cpp

--- NocticOS.Apps/PkgInstaller/pkginstaller.cpp
+++ NocticOS.Apps/PkgInstaller/pkginstaller.cpp
@@ -6,8 +6,26 @@
 
 void main()
 {
+	string option = "";
+
+	string options[2] = {
+		"[1] Install",
+		"[2] Uninstall"
+	};
+
+	for (int i = 0; i < 2; i++) {
+		cout << options[i];
+	}
+
+}
+
+void StartInstall()
+{
 	string repoLink = "";
 	cout << "Enter a repository link to install: ";
 	cin >> repoLink;
 	InstallPackage(repoLink);
 }
+
+void StartUninstall()
+{}
