f75fc37f76913d0e35b2347f7045541228b2c6d3
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-18 19:31:47 +0800

    Create VMath.h

---
+++ NocticOS.Applications/V-Script/include/VMath.h
@@ -0,0 +1,29 @@
+// VMath.h
+
+#include<iostream>
+
+using namespace std;
+
+class VMath
+{
+	public:
+		// Addition
+		void add(int a, int b) {
+			return a + b;
+		}
+
+		// Subtraction
+		void sub(int a, int b) {
+			return a - b;
+		}
+
+		// Multiplication
+		void mul(int a, int b) {
+			return a * b;
+		}
+
+		// Division
+		void div(int a, int b) {
+			return a / b;
+		}
+};
