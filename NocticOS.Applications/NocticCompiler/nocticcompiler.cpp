1d5c0d47b5d87d2be87cd89c472e7394b55a7a4a
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-19 12:17:06 +0800

    Update nocticcompiler.cpp

--- NocticOS.Applications/NocticCompiler/nocticcompiler.cpp
+++ NocticOS.Applications/NocticCompiler/nocticcompiler.cpp
@@ -1,3 +1,7 @@
+/*
+New way of compiling NocticOS.
+*/
+
 // nocticcompiler.cpp
 
 #include<iostream>
@@ -6,6 +10,29 @@ using namespace std;
 
 int main()
 {
-	// No code yet.
+	int curOption;
+
+	string compileOptions[2] = {
+		"[1] Compile NocticOS",
+		"[2] Compile C++ files",
+		"[3] Run NocticOS Updater"
+	};
+
+	for (int i = 0; i < 3; i++) {
+		cout << compileOptions;
+	}
+
+	cin >> curOption;
+
+	switch(curOption)
+	{
+		case 1:
+		case 2:
+		case 3:
+	}
+
+	cout << "\n";
+	system("pause");
+
 	return 0;
 }
