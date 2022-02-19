11e635d2b87fbbb2a6341c282ca9d40fc5a4d2d3
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-19 13:59:46 +0800

    Update nocticupdater.cpp

--- NocticOS.Applications/NocticCompiler/nocticupdater.cpp
+++ NocticOS.Applications/NocticCompiler/nocticupdater.cpp
@@ -1,11 +1,25 @@
 // nocticupdater.cpp
 
+#include "include/updateproj.h"
 #include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-	// No code yet
+	string curOption;
+	cout << "Run the update process? [y/n]: ";
+	cin >> curOption;
+
+	if (curOption == "y" || curOption == "Y") {
+
+	} else if (curOption == "n" || curOption == "N") {
+
+	} else {
+		cout << "Unknown Character.\n";
+		system("pause");
+	}
+
 	return 0;
 }
