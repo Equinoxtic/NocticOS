ea08fd00472d6112ff4b880a46ae44910b063db4
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-19 16:19:32 +0800

    Update compilecppfiles.h

--- NocticOS.Applications/NocticCompiler/include/compilecppfiles.h
+++ NocticOS.Applications/NocticCompiler/include/compilecppfiles.h
@@ -3,3 +3,14 @@
 #include<iostream>
 
 using namespace std;
+
+void CompileCPPFiles()
+{
+	system("cd..");
+	system("cd..");
+	system("cd PkgInstaller");
+	system("g++ pkginstaller.cpp -o pkginstaller.exe");
+	system("cd..");
+	system("cd V-Script");
+	system("g++ vscript.cpp -o vscript.exe");
+}
