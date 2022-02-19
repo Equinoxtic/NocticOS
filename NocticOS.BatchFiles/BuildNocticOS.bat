66ca612ae4ff1b09f6b9b749a55471fbcb5458d4
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-19 16:06:08 +0800

    Update BuildNocticOS.bat

--- NocticOS.BatchFiles/BuildNocticOS.bat
+++ NocticOS.BatchFiles/BuildNocticOS.bat
@@ -11,7 +11,9 @@ SET /P PROMPT_MSG=Would you like to build NocticOS? [y/n]:
 IF %PROMPT_MSG%==y (
 	ECHO:
 	cd..
-	dotnet build
+	cd NocticOS.Applications/NocticCompiler
+	g++ nocticcompiler.cpp -o nocticcompiler.exe
+	g++ nocticupdater.cpp -o nocticupdater.exe
 	PAUSE
 ) ELSE IF %PROMPT_MSG%==n (
 	ECHO:
