38ff7fb9aed0a70fb5c510e1271aaeb143896fa6
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-19 16:30:22 +0800

    Minor fix.

--- NocticOS.Applications/NocticCompiler/include/updateproj.h
+++ NocticOS.Applications/NocticCompiler/include/updateproj.h
@@ -6,7 +6,7 @@ using namespace std;
 
 void UpdateProject()
 {
-	cout << "NOTICE: This will get the LATEST version of NocticOS."
+	cout << "NOTICE: This will get the LATEST version of NocticOS.";
 	string curRepo = "https://github.com/Equinoxtic/NocticOS";
 	string str = "git ";
 	str = str + "clone " + curRepo;
