9d5df585aeb8b21fc5e766d4c78bcad82b2d58a9
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-26 02:59:17 +0800

    Create FunctionCodePresets.cs

---
+++ NocticOS.Source/FunctionCodePresets.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Source
+{
+    public class FunctionCodePresets
+    {
+        public void CodePresetSwitch(String curCodePreset)
+        {
+            // No code yet.
+        }
+    }
+}
