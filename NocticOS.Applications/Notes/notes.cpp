270649ddc24969301a154f5f87c2448928b48e16
VerticalMoron <ThisIsEquinoctic@gmail.com>
2022-02-28 03:01:44 +0800

    Create notes.cpp

---
+++ NocticOS.Applications/Notes/notes.cpp
@@ -0,0 +1,15 @@
+// notes.cpp
+
+#include<iostream>
+#include "include/returnnotes.h"
+
+using namespace std;
+
+int main(bool wantsReturn)
+{
+	if (wantsReturn == true) {
+		DoReturn();
+	}
+
+	return 0;
+}
