f97aae74bedeae0293f444e37ef041aa3858ccc6
VerticalMoron <ThisIsEquinoctic@gmail.com>
2022-02-28 02:47:04 +0800

    Create TemplateClass.cs

---
+++ NocticOS.Templates/CS-Class-Templates/TemplateClass.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NocticOS;
+
+namespace NamespaceTemplate
+{
+	class TemplateFile
+	{
+		public void TemplateFunc()
+		{
+			ProgramReturn Program = new ProgramReturn();
+
+			/* Code */
+
+			Program.DoReturn(false);
+		}
+	}
+}
