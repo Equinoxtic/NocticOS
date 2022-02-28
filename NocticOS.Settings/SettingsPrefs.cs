f201652c87f3d77baf99c971b1f81a39dc6f2746
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-28 13:31:39 +0800

    Implement class: `DebugPrefs`

--- NocticOS.Settings/SettingsPrefs.cs
+++ NocticOS.Settings/SettingsPrefs.cs
@@ -10,4 +10,28 @@ namespace NocticOS.Settings
         public static string currentConsoleFG = "";
         public static string currentConsoleBG = "";
     }
+
+    public class DebugPrefs
+    {
+        public void SetDebugProperty(string dbgLevel, bool allowDebug)
+        {
+            if (allowDebug) {
+                switch(dbgLevel) {
+                    case "guest-debug":
+                        dbgLevel = "guest";
+                        break;
+
+                    case "debug":
+                        dbgLevel = "debug";
+                        break;
+
+                    case "debug-admin":
+                        dbgLevel = "admin";
+                        break;
+                }
+            } else {
+                dbgLevel = "none";
+            }
+        }
+    }
 }
