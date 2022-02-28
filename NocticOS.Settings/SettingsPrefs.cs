aba62e4c902c8854929e8921d8816f74fee39aa2
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-28 13:31:55 +0800

    Implement class: `SystemPrefs`

--- NocticOS.Settings/SettingsPrefs.cs
+++ NocticOS.Settings/SettingsPrefs.cs
@@ -34,4 +34,24 @@ namespace NocticOS.Settings
             }
         }
     }
+
+    public class SystemPrefs
+    {
+        public static string currentSystem = "";
+        public static string currentSystemKey = "";
+        public static string currentSystemVersion = "";
+
+        public void GetVersion(string sysname, string sysversion, string syskey)
+        {
+            string[] versionls = {
+                sysname,
+                sysversion,
+                syskey
+            };
+            
+            for (int i = 0; i < versionls.Length; i++) {
+                Console.Write(versionls[i] + "\n");
+            }
+        }
+    }
 }
