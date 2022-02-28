c07d6c9b9df94760a992aff244b3a64d9fbfb61f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-28 12:05:59 +0800

    Update notes.cpp

--- NocticOS.Applications/Notes/notes.cpp
+++ NocticOS.Applications/Notes/notes.cpp
@@ -2,11 +2,14 @@
 
 #include<iostream>
 #include "include/returnnotes.h"
+#include "include/taskbar.h"
 
 using namespace std;
 
 int main(bool wantsReturn)
 {
+	CreateTaskbar();
+	
 	if (wantsReturn == true) {
 		DoReturn();
 	}
