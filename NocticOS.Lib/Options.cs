7c3ce1eb9ef1acda2a1a74ecc2d41537dc039508
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-01 21:05:40 +0800

    Minor fix.

--- NocticOS.Lib/Options.cs
+++ NocticOS.Lib/Options.cs
@@ -12,7 +12,7 @@ namespace NocticOS.Lib
         public void PushOptions(string? daName, int? setLength, string[] setArr, string[] setArrTag) {
             string[] curArr = setArr;
             string[] curArrTags = setArrTag;
-            if (String.IsNullOrEmpty(daName)) {
+            if (!String.IsNullOrEmpty(daName)) {
                 for (int i = 0; i < setLength; i++) {
                     Console.Write(curArrTags[i] + curArr[i] + "\n");
                 }
