3dab4bd2da8f6ba11dc815bb6710c7040b8c913c
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-01 16:48:31 +0800

    Create CreatedClass.cs

---
+++ NocticOS.Lib/CreatedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Lib
+{
+	public static class CreatedClass
+	{
+		public string? name;
+		public string? tag;
+		public void CreateClass(string? curName, string? curTag)
+		{
+			// No code yet.
+		}
+	}
+}
