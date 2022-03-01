33bc85efd93b7c1c309ecbfc1c3c9deaef52d26d
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-01 21:31:28 +0800

    Update DateDisplayHelp.cs

--- NocticOS.HelpUtil/DateDisplayHelp.cs
+++ NocticOS.HelpUtil/DateDisplayHelp.cs
@@ -34,7 +34,7 @@ namespace NocticOS.HelpUtil
 
             Console.Write("\n");
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < dateDisplayLs.Length; i++) {
                 if (isMissingParams) {
                     Console.Write("day " + dateDisplayLs[i] + "\n");
                 } else {
