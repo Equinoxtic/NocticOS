86f82cd014cabe0cfa6431bcef6e7d09b562d132
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-01 21:45:34 +0800

    Update CreationHelp.cs

--- NocticOS.HelpUtil/CreationHelp.cs
+++ NocticOS.HelpUtil/CreationHelp.cs
@@ -8,27 +8,28 @@ namespace NocticOS.HelpUtil
     public class CreationHelp
     {
         ThrowError throwError = new ThrowError();
+
+        string[] creationLs = {
+            "-help - Shows information about the 'create' command.",
+            "-sel - Standard Creation selection, gives you the options for creating a custom object, function and class.",
+            "-obj - Create a Custom Object, input it's own name and type.",
+            "-func - Create a Custom Function, input it's own name and allows you to select a code preset to run.",
+            "-class - Create a Custom Class, input it's own name and it also allows you to store objects within it."
+        };
+
+        /*
+        string[] creationDescs = {
+            " - Shows information about the 'create' command.",
+            " - Standard Creation selection, gives you the options for creating a custom object, function and class.",
+            " - Create a Custom Object, input it's own name and type.",
+            " - Create a Custom Function, input it's own name and allows you to select a code preset to run.",
+            " - Create a Custom Class, input it's own name and it also allows you to store objects within it.",
+        };
+        */
+
         public void OpenHelp(bool isMissingParams)
         {
             ProgramReturn Program = new ProgramReturn();
-
-            string[] creationLs = {
-                "-help - Shows information about the 'create' command.",
-                "-sel - Standard Creation selection, gives you the options for creating a custom object, function and class.",
-                "-obj - Create a Custom Object, input it's own name and type.",
-                "-func - Create a Custom Function, input it's own name and allows you to select a code preset to run.",
-                "-class - Create a Custom Class, input it's own name and it also allows you to store objects within it."
-            };
-
-            /*
-            string[] creationDescs = {
-                " - Shows information about the 'create' command.",
-                " - Standard Creation selection, gives you the options for creating a custom object, function and class.",
-                " - Create a Custom Object, input it's own name and type.",
-                " - Create a Custom Function, input it's own name and allows you to select a code preset to run.",
-                " - Create a Custom Class, input it's own name and it also allows you to store objects within it.",
-            };
-            */
 
             if (isMissingParams) {
                 throwError.MissingParamsThrow(true);
