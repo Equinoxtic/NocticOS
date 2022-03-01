2a8165253a69345617345404038c7db80b3bbb23
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-01 21:31:18 +0800

    Update DateDisplayHelp.cs

--- NocticOS.HelpUtil/DateDisplayHelp.cs
+++ NocticOS.HelpUtil/DateDisplayHelp.cs
@@ -13,18 +13,20 @@ namespace NocticOS.HelpUtil
             ProgramReturn Program = new ProgramReturn();
 
             string[] dateDisplayLs = {
-                "-t",
-                "-help",
-                "-d",
-                "-cur"
+                "-t - Displays the current time.",
+                "-help - Shows the available commands for 'day'",
+                "-d - Displays the current date.",
+                "-cur - Displays the current month, date and time."
             };
 
+            /*
             string[] dateDisplayDescs = {
                 " - Displays the current time.",
                 " - Shows the available commands for 'day'",
                 " - Displays the current date.",
                 " - Displays the current month, date and time."
             };
+            */
 
             if (isMissingParams) {
                 throwError.MissingParamsThrow(true);   
@@ -34,9 +36,9 @@ namespace NocticOS.HelpUtil
 
             for (int i = 0; i < 4; i++) {
                 if (isMissingParams) {
-                    Console.Write("day " + dateDisplayLs[i] + dateDisplayDescs[i] + "\n");
+                    Console.Write("day " + dateDisplayLs[i] + "\n");
                 } else {
-                    Console.Write(dateDisplayLs[i] + dateDisplayDescs[i] + "\n");
+                    Console.Write(dateDisplayLs[i] + "\n");
                 }
             }
 
