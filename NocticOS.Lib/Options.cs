0f2c06ae29c85072827c4c48acfd4519006b274b
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-01 21:04:46 +0800

    Create Options.cs

---
+++ NocticOS.Lib/Options.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NocticOS.Utility;
+
+namespace NocticOS.Lib
+{
+    public class Options
+    {
+        ThrowError throwError = new ThrowError();
+        
+        public void PushOptions(string? daName, int? setLength, string[] setArr, string[] setArrTag) {
+            string[] curArr = setArr;
+            string[] curArrTags = setArrTag;
+            if (String.IsNullOrEmpty(daName)) {
+                for (int i = 0; i < setLength; i++) {
+                    Console.Write(curArrTags[i] + curArr[i] + "\n");
+                }
+            } else {
+                throwError.CustomThrow("ERROR: Name is empty.\n", true);
+            }
+        }
+    }
+}
