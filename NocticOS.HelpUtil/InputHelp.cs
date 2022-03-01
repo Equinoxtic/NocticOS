d83cdb53a5a1cfcdb7a7f9c897cabec285f14b07
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-01 21:45:40 +0800

    Update InputHelp.cs

--- NocticOS.HelpUtil/InputHelp.cs
+++ NocticOS.HelpUtil/InputHelp.cs
@@ -8,27 +8,30 @@ namespace NocticOS.HelpUtil
     public class InputHelp
     {
         ThrowError throwError = new ThrowError();
+
+        string[] InputCmdLs = {
+            "-help - Shows help and information for the 'input' command.",
+            "-sel - Select what type you should input.",
+            "-str - Standard string input.",
+            "-int - Standard float input.",
+            "-flt - Standard float input.",
+            "-dbl - Standard double input."
+        };
+
+        /*
+        string[] InputCmdDescs = {
+            " - Shows help and information for the 'input' command.",
+            " - Select what type you should input.",
+            " - Standard string input.",
+            " - Standard integer input.",
+            " - Standard float input.",
+            " - Standard double input."
+        };
+        */
+        
         public void OpenHelp(bool isMissingParams)
         {
             ProgramReturn Program = new ProgramReturn();
-
-            string[] InputCmdLs = {
-                "-help",
-                "-sel",
-                "-str",
-                "-int",
-                "-flt",
-                "-dbl"
-            };
-
-            string[] InputCmdDescs = {
-                " - Shows help and information for the 'input' command.",
-                " - Select what type you should input.",
-                " - Standard string input.",
-                " - Standard integer input.",
-                " - Standard float input.",
-                " - Standard double input."
-            };
 
             if (isMissingParams) {
                 throwError.MissingParamsThrow(true);
@@ -36,8 +39,8 @@ namespace NocticOS.HelpUtil
 
             Console.Write("\n");
 
-            for (int i = 0; i < 6; i++) {
-                Console.Write("input " + InputCmdLs[i] + InputCmdDescs[i] + "\n");
+            for (int i = 0; i < InputCmdLs.Length; i++) {
+                Console.Write("input " + InputCmdLs[i] + "\n");
             }
 
             Program.DoReturn(false);
