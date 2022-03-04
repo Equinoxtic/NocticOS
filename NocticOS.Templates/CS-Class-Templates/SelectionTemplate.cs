936b91b1ed4b0f704c6fed7c2f102eae56faff6d
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-05 05:38:46 +0800

    Update SelectionTemplate.cs

--- NocticOS.Templates/CS-Class-Templates/SelectionTemplate.cs
+++ NocticOS.Templates/CS-Class-Templates/SelectionTemplate.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using NocticOS.Utility;
+using NocticOS.Lib;
 using NocticOS;
 
 namespace NamespaceTemplate
@@ -11,14 +12,19 @@ namespace NamespaceTemplate
         public void SelectName(bool isMissingParams)
         {
             ProgramReturn Program = new ProgramReturn();
-            ThrowError throwError = new ThrowError();
+            Throw missingParams = new Throw();
+            // ThrowError throwError = new ThrowError();
 
             string[] selectLs = {
                 "cur - desc"
             };
 
             if (isMissingParams) {
-                throwError.MissingParamsThrow(true);
+                // throwError.MissingParamsThrow(true);
+                missingParams.CreateThrow("mp",
+                    "Missing Parameters. Please enter the following:",
+                    true
+                );
             }
 
             for (int i = 0; i < selectLs.Length; i++) {
