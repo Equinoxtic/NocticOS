bc264367724684d8cd202e48c3d10ca5d92b5a7b
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-05 14:44:31 +0800

    Set `cmd` parameter as optional.

--- NocticOS.Lib/Options.cs
+++ NocticOS.Lib/Options.cs
@@ -10,16 +10,15 @@ namespace NocticOS.Lib
         // ThrowError throwError = new ThrowError();
         Throw emptyThrow = new Throw();
         
-        public void PushOptions(string? cmd, string[] setArr, int? setLength) {
+        public void PushOptions(string[] setArr, int? setLength, string? cmd="") {
             if (!String.IsNullOrEmpty(cmd)) {
                 for (int i = 0; i < setLength; i++) {
                     Console.Write(cmd + " " + setArr[i] + "\n");
                 }
             } else {
-                emptyThrow.CreateThrow("et",
-                    "ERROR: Empty Command name.",
-                    true
-                );
+                for (int i = 0; i < setLength; i++) {
+                    Console.Write(setArr[i] + "\n");
+                }
             }
         }
     }
