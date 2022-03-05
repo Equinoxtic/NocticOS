31d02f57480c46d9daae7e6dfc2995cca7254bf1
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-05 15:52:24 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using NocticOS.Main;
-using NocticOS.Source;
-using NocticOS.HelpUtil;
-using NocticOS.Utility;
+using NocticOS.Lib;
 
 namespace NocticOS
 {
@@ -12,76 +9,17 @@ namespace NocticOS
 	{
 		public static void Main(String[] args)
 		{
-			// Help Utils
-			BuildHelp buildHelp = new BuildHelp();
-			HelpState helpState = new HelpState();
-			DateDisplayHelp dateDisplayHelp = new DateDisplayHelp();
-			MathHelp mathHelp = new MathHelp();
-			InputHelp inputHelp = new InputHelp();
-			CreationHelp creationHelp = new CreationHelp();
-
-			// Utils
-			DirectoryBuilder directoryBuilder = new DirectoryBuilder();
-			OSBuilder oSBuilder = new OSBuilder();
-			DesktopEnviromentBuilder deBuilder = new DesktopEnviromentBuilder();
-			DateDisplay dateDisplayer = new DateDisplay();
-			StandardMath standardMath = new StandardMath();
-
-			// States /  Classes
-			InputState standardInput = new InputState();
-			MathState mathState = new MathState();
-			CreationState creationState = new CreationState();
+			CommandSwitch cmdSwitch = new CommandSwitch();
 
 			var curSelected = "";
 			
 			Console.WriteLine("\nNocticOS - Version (1.1.5)");
 			Console.Write("Type in 'help' to get started.");
 			Console.Write("\n> ");
+
 			curSelected = Console.ReadLine();
 
-			switch (curSelected)
-			{
-				case "help" : helpState.OpenState(); break;
-
-				case "day" : dateDisplayHelp.OpenHelp(true); break;
-				case "day -help" : dateDisplayHelp.OpenHelp(false); break;
-				case "day -t" : dateDisplayer.DisplayTime(); break;
-				case "day -d" : dateDisplayer.DisplayDate(); break;
-				case "day -cur" : dateDisplayer.DisplayTimeAndDate(); break;
-
-				case "input" : inputHelp.OpenHelp(true); break;
-				case "input -help" : inputHelp.OpenHelp(false); break;
-				case "input -sel" : standardInput.InputSelect(); break;
-				case "input -str" : standardInput.InputSwitch("str"); break;
-				case "input -flt" : standardInput.InputSwitch("flt"); break;
-				case "input -int" : standardInput.InputSwitch("int"); break;
-				case "input -dbl" : standardInput.InputSwitch("dbl"); break;
-
-				case "math" : mathHelp.OpenHelp(true); break;
-				case "math -help" : mathHelp.OpenHelp(false); break;
-				case "math -new" : mathState.OpenState(); break;
-				case "math -add" : standardMath.NocticMath("add", true); break;
-				case "math -subtract" : standardMath.NocticMath("subtract", true); break;
-				case "math -multiply" : standardMath.NocticMath("multiply", true); break;
-				case "math -divide" : standardMath.NocticMath("divide", true); break;
-
-				case "create" : creationHelp.OpenHelp(true); break;
-				case "create -help" : creationHelp.OpenHelp(false); break;
-				case "create -sel" : creationState.OpenState(); break;
-				case "create -obj" : creationState.CreationSwitch("obj"); break;
-				case "create -func" : creationState.CreationSwitch("func"); break;
-				case "create -class" : creationState.CreationSwitch("class"); break;
-				case "create -struct" : creationState.CreationSwitch("struct"); break;
-				case "create -inface" : creationState.CreationSwitch("inface"); break;
-
-				case "build" : buildHelp.OpenHelp(true); break;
-				case "build -help" : buildHelp.OpenHelp(false); break;
-				case "build -dirs" : directoryBuilder.DirBuild(); break;
-				case "build -os" : oSBuilder.BuildOS(); break;
-				case "build -de" : deBuilder.DEBuild(); break;
-
-				case "exit" : Console.WriteLine("\nExiting program...\n"); Thread.Sleep(3000); break;
-			}
+			cmdSwitch.OpenCommand(curSelected);
 		}
 	}
 }
