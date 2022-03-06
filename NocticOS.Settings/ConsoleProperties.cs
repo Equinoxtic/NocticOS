b5e7776f21c95367566fc3924b1b7cf6d008c5b9
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-06 21:43:46 +0800

    Update ConsoleProperties.cs

--- NocticOS.Settings/ConsoleProperties.cs
+++ NocticOS.Settings/ConsoleProperties.cs
@@ -6,13 +6,6 @@ namespace NocticOS.Settings
 {
     public class ConsoleProperties
     {
-        ConsolePrefs consolePrefs = new ConsolePrefs();
-
-        public void ConsoleSettingsState()
-        {
-            Program program = new Program();
-
-            program.DoReturn(false);
-        }
+        // No code.
     }
 }
