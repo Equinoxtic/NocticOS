e473f2f3de517ce357d0e7dab0745efe0a6c910d
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-18 14:51:39 +0800

    Update MainConsole.cs

--- NocticOS.Source/MainConsole.cs
+++ NocticOS.Source/MainConsole.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NocticOS.Lib;
+
 namespace NocticOS.Source
 {
     public class MainConsole
     {
-        
+        static string mainPath = "Home/";
+        static string choice = "";
+        ConsoleCommandSwitch consoleCommandSwitch = new ConsoleCommandSwitch();
+        public void OpenConsole()
+        {   
+            Console.Write("\n" + mainPath + "> ");
+            choice = Console.ReadLine();
+            consoleCommandSwitch.DoSwitch(choice);
+        }
     }
 }
