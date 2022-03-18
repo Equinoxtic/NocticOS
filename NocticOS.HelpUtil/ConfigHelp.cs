f258a6b7b4217280d2dfbcc940ab6b2dcb046f88
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-18 18:20:48 +0800

    Create ConfigHelp.cs

---
+++ NocticOS.HelpUtil/ConfigHelp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.HelpUtil
+{
+    public class ConfigHelp
+    {
+        public void OpenHelp(bool isMissingParams)
+        {
+            Program program = new Program();
+
+            program.DoReturn(false);
+        }
+    }
+}
