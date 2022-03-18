520db43c67885c0f1ae402d51af1d1ba89e6c94b
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-18 14:56:15 +0800

    Update ConsoleCommandSwitch.cs
    Add exit commands

--- NocticOS.Lib/ConsoleCommandSwitch.cs
+++ NocticOS.Lib/ConsoleCommandSwitch.cs
@@ -6,11 +6,12 @@ namespace NocticOS.Lib
 {
     public class ConsoleCommandSwitch
     {
+        Program program = new Program();
         public void DoSwitch(string choice)
         {
             switch (choice)
             {
-            
+                case "exit" or "out" or "back": program.DoReturn(false); break;
             }
         }
     }
