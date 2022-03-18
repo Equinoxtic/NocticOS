29e0177955877e31e05a4aeea1524a4e80da3417
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-18 18:26:18 +0800

    Update ConfigHelp.cs
    Now shows configuration command list.

--- NocticOS.HelpUtil/ConfigHelp.cs
+++ NocticOS.HelpUtil/ConfigHelp.cs
@@ -1,14 +1,38 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using NocticOS.Lib;
 
 namespace NocticOS.HelpUtil
 {
     public class ConfigHelp
     {
+        Throw missingParamsThrow = new Throw();
+
+        string[] configHelpLs = {
+            "-bg - Background Color configuration.",
+            "-fg - Foreground Color configuration.",
+            "-cursor - Cursor configuration."
+        };
+
         public void OpenHelp(bool isMissingParams)
         {
             Program program = new Program();
+
+            if (isMissingParams) {
+                missingParamsThrow.CreateThrow(
+                    false,
+                    "mp",
+                    "Missing parameters, please enter the following:",
+                    true
+                );
+            }
+
+            Console.Write("\n");
+
+            for (int i = 0; i < configHelpLs.Length; i++) {
+                Console.Write("config " + configHelpLs[i] + "\n");
+            }
 
             program.DoReturn(false);
         }
