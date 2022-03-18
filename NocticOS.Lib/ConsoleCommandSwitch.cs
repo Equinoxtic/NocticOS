d78a8cd8f596968b4826a81970c430c56748a870
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-18 14:51:35 +0800

    Create ConsoleCommandSwitch.cs

---
+++ NocticOS.Lib/ConsoleCommandSwitch.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Lib
+{
+    public class ConsoleCommandSwitch
+    {
+        public void DoSwitch(string choice)
+        {
+            switch (choice)
+            {
+            
+            }
+        }
+    }
+}
