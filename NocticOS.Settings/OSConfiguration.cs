33faa97124c39d047c4cdb0685c0f3ba3c11c4f6
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-18 15:46:55 +0800

    Create OSConfiguration.cs

---
+++ NocticOS.Settings/OSConfiguration.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NocticOS.Lib;
+
+namespace NocticOS.Settings
+{
+    public class OSConfiguration
+    {
+        Options choices = new Options();
+        ConsoleProperties consoleProperties = new ConsoleProperties();
+        SystemProperties systemProperties = new SystemProperties();
+
+        static string choice = "";
+        static string[] choiceArray = {
+            ""
+        };
+        static string[] choiceDescs = {
+            ""
+        };
+
+        public void Open(bool isList)
+        {
+            Console.Write("\nSelect configuration:\n");
+            choice = Console.ReadLine();
+            if (isList) {
+                choices.PushOptions(choiceArray, choiceArray.Length, "config");
+            } else {
+                for (int i = 0; i < choiceArray.Length; i++) {
+                    Console.Write("[" + choiceArray[i] + "]" + " - " + choiceDescs + "\n");
+                }
+            }
+        }
+        
+        static void SwitchConfigState(string choice)
+        {
+            switch (choice)
+            {
+                case "config -bg" or "-bg":
+            }
+        }
+
+        static string[] colorChoices = {
+            "Black",
+            "Dark Blue",
+            "Dark Green",
+            "Dark Cyan",
+            "Dark Red",
+            "Dark Magenta",
+            "Dark Yellow",
+            "Gray",
+            "Dark Gray",
+            "Blue",
+            "Green",
+            "Cyan",
+            "Red",
+            "Magenta",
+            "Yellow",
+            "White"
+        };
+
+        static void BGorFGconfig(string type) {
+            BackgroundSwitch bgSwitch = new BackgroundSwitch();
+            string kw = "";
+            int choice = 0;
+            switch (type) {
+                case "bg":
+                    kw = "Background";
+                    break;
+                case "fg":
+                    kw = "Foreground";
+                    break;
+            }
+            Console.Write(kw + " color configuration, input your desired color.\n");
+            for (int i = 0; i < colorChoices.Length; i++) {
+                Console.Write("[" + i + "]" + " - " + colorChoices[i] + "\n");
+            }
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (kw)
+            {
+                case "Background":
+                    bgSwitch.BGSwitch(choice);
+                break;
+            }
+        }
+    }
+}
