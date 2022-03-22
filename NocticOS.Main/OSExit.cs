953a0f35fc34ae81349c552fdee2725685533905
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-23 01:01:00 +0800

    Update OSExit.cs

--- NocticOS.Main/OSExit.cs
+++ NocticOS.Main/OSExit.cs
@@ -6,6 +6,13 @@ namespace NocticOS.Main
 {
     public class OSExit
     {
-        
+        public void Exit(bool shouldClearScreen) {
+            ClearScreen clearScreen = new ClearScreen();
+            Console.Write("\nExiting...\n");
+            Thread.Sleep(3000);
+            if (shouldClearScreen) {
+                clearScreen.ScreenClear(false);
+            }
+        }
     }
 }
