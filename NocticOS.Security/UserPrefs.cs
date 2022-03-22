07a7f1e2aaa47c15f59779706450c39bdac8fb41
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-22 11:54:13 +0800

    Create UserPrefs.cs

---
+++ NocticOS.Security/UserPrefs.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Security
+{
+    public class UserPrefs
+    {
+        string username = "";
+        string userpw = "";
+    }
+}
