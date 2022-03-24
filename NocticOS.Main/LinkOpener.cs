92e32284426ab9538459389c00aeeced7f6f36af
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-24 17:59:09 +0800

    Update LinkOpener.cs

--- NocticOS.Main/LinkOpener.cs
+++ NocticOS.Main/LinkOpener.cs
@@ -1,13 +1,44 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using NocticOS.Lib;
 
 namespace NocticOS.Main
 {
 	public class LinkOpener
 	{
-		public void OpenLink(string url = "")
+		public void OpenLink(string? url = "")
 		{
+			Program program = new Program();
+			if (!String.IsNullOrEmpty(url)) {
+				url = url.Replace("&", "&^");
+				Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+			} else {
+				LinkOpenPrompt();
+			}
+			program.DoReturn(false);
+		}
+
+		void LinkOpenPrompt()
+		{
+			Program program = new Program();
+			string? url = "";
+			Console.Write("\nInput a URL:\n");
+			url = Console.ReadLine();
+			if (String.IsNullOrEmpty(url)) {
+				Throw emptyValue = new Throw();
+				emptyValue.CreateThrow(
+					false,
+					"ev",
+					"Empty value. Please try again.",
+					true
+				);
+				program.DoReturn(false);
+			} else {
+				url = url.Replace("&", "&^");
+				Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+			}
 		}
 	}
 }
