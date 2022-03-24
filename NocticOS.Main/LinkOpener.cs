25564b2e142b3c979c18693c4968066141646a03
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-24 17:32:05 +0800

    Create LinkOpener.cs

---
+++ NocticOS.Main/LinkOpener.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Main
+{
+	public class LinkOpener
+	{
+		public void OpenLink(string url = "")
+		{
+		}
+	}
+}
