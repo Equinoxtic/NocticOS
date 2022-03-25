9b63bfa317796b998bb5bd7e4b62041439e8701c
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-25 12:28:47 +0800

    Update SecretSwitcher.cs

--- NocticOS.Secrets/SecretSwitcher.cs
+++ NocticOS.Secrets/SecretSwitcher.cs
@@ -1,17 +1,35 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using NocticOS.Lib;
 
 namespace NocticOS.Secrets
 {
 	public class SecretSwitcher
 	{
+		FunIsInfinite funIsInfinite = new FunIsInfinite();
 		public void SecretSwitch(string? secret = "")
 		{
+			Program program = new Program();
 			if (String.IsNullOrEmpty(secret)) {
-
+				Throw emptyValue = new Throw();
+				emptyValue.CreateThrow(
+					false,
+					"ev",
+					"Empty value. Please try again.",
+					true
+				);
+				program.DoReturn(false);
 			} else {
-				
+				GoToSecret(secret);
+			}
+		}
+
+		void GoToSecret(string? a) {
+			switch(a) {
+				case "infinite":
+					funIsInfinite.Open();
+					break;
 			}
 		}
 	}
