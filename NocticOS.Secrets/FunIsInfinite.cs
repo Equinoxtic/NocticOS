a03a4fa0f70da2d5a02c52eac8b663c2f4598471
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-25 12:28:22 +0800

    Create FunIsInfinite.cs

---
+++ NocticOS.Secrets/FunIsInfinite.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Secrets
+{
+	public class FunIsInfinite
+	{
+		public void Open()
+		{
+			Program program = new Program();
+			Console.Write("\nFun is infinite. -Majin\n");
+			program.DoReturn(false);
+		}
+	}
+}
