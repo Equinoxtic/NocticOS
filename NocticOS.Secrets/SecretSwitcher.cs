cafa0e304ea334be51682926ff69a9fd98abbeb7
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-25 12:18:57 +0800

    Create SecretSwitcher.cs

---
+++ NocticOS.Secrets/SecretSwitcher.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Secrets
+{
+	public class SecretSwitcher
+	{
+		public void SecretSwitch(string? secret = "")
+		{
+			if (String.IsNullOrEmpty(secret)) {
+
+			} else {
+				
+			}
+		}
+	}
+}
