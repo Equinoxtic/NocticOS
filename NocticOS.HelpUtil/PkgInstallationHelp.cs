9cc5b54f0f9cb5909157ad230d27b994af89f637
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-28 01:56:45 +0800

    Create PkgInstallationHelp.cs

---
+++ NocticOS.HelpUtil/PkgInstallationHelp.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NocticOS.Lib;
+
+namespace NocticOS.HelpUtil
+{
+	public class PkgInstallationHelp
+	{
+		string[] pkgInstallLs = {
+			"-help ::  Displays the information for the 'pkg' command.",
+			"-install :: Standard Installation Prompt."
+		};
+		
+		public void OpenPkgInstallerHelp(bool isMissingParams)
+		{
+			Program program = new Program();
+			Options pkgInstallOpts = new Options();
+			Throw missingParams = new Throw();
+
+			if (!isMissingParams) {
+				Console.Write("\nPackage Installer Commands:\n\n");
+				pkgInstallOpts.PushOptions(pkgInstallLs, pkgInstallLs.Length, true);
+			} else {
+				missingParams.CreateThrow(
+					false,
+					"mp",
+					"Missing Parameters, please enter the following and try again:\n\n",
+					true
+				);
+				pkgInstallOpts.PushOptions(pkgInstallLs, pkgInstallLs.Length, true, "pkg");
+			}
+
+			program.DoReturn(false);
+		}
+	}
+}
