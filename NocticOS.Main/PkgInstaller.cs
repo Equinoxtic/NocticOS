cb4908167c95b651a17d0c90eb342a70ff379663
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-28 01:35:03 +0800

    Update PkgInstaller.cs
    Woohoo! Package managing!

--- NocticOS.Main/PkgInstaller.cs
+++ NocticOS.Main/PkgInstaller.cs
@@ -5,19 +5,48 @@ Pretty much the same as the "LinkOpener" class
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using NocticOS.Lib;
+using LibGit2Sharp;
 
 namespace NocticOS.Main
 {
 	public class PkgInstaller
 	{
-		public void InstallPackage(string? pkgUrl)
+		public void InstallPackage(string? pkgUrl = "", string? path = "")
 		{
-
+			Program program = new Program();
+			if (!String.IsNullOrEmpty(pkgUrl) || !String.IsNullOrEmpty(path)) {
+				Repository.Clone(pkgUrl, path);
+			} else {
+				InstallPrompt();
+			}
+			program.DoReturn(false);
 		}
 		
 		void InstallPrompt()
 		{
-
+			Program program = new Program();
+			string? pkgUrl = "";
+			string? path = "";
+			Console.Write("\nInput the package you want to install: ");
+			pkgUrl = Console.ReadLine();
+			Console.Write("\nInput the path for the repository to be cloned to: ");
+			path = Console.ReadLine();
+			if (String.IsNullOrEmpty(pkgUrl) || String.IsNullOrEmpty(path)) {
+				Throw nullUrl = new Throw();
+				nullUrl.CreateThrow(
+					false,
+					"nu",
+					"Null / Empty URL, please input the URL again, later.",
+					true
+				);
+				program.DoReturn(false);
+			} else {
+				Repository.Clone(pkgUrl, path);
+			}
+			Console.Write("\nSuccessfully installed package.\n");
+			program.DoReturn(false);
 		}
 	}
 }
