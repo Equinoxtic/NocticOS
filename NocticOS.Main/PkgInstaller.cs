186f89cb91167222bcdc1dbc937545eea537da28
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-28 01:11:03 +0800

    Create PkgInstaller.cs

---
+++ NocticOS.Main/PkgInstaller.cs
@@ -0,0 +1,23 @@
+/*
+Pretty much the same as the "LinkOpener" class
+*/
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Main
+{
+	public class PkgInstaller
+	{
+		public void InstallPackage(string? pkgUrl)
+		{
+
+		}
+		
+		void InstallPrompt()
+		{
+
+		}
+	}
+}
