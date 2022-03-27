799b26b3f3688c272d64d74bda1d5678f94874cd
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-27 17:23:53 +0800

    Update FunIsInfinite.cs

--- NocticOS.Secrets/FunIsInfinite.cs
+++ NocticOS.Secrets/FunIsInfinite.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using NocticOS.Lib;
+using NocticOS.Settings;
 
 namespace NocticOS.Secrets
 {
@@ -9,8 +11,24 @@ namespace NocticOS.Secrets
 		public void Open()
 		{
 			Program program = new Program();
+			MajinCodeSwitcher majinCodeSwitcher = new MajinCodeSwitcher();
+			CursorPrefs cursorPrefs = new CursorPrefs();
+			string? secretCode = "";
 			Console.Write("\nFun is infinite. -Majin\n");
-			program.DoReturn(false);
+			Console.Write("\n" + cursorPrefs.curSelectCursor);
+			secretCode = Console.ReadLine();
+			if (String.IsNullOrEmpty(secretCode)) {
+				Throw emptyValue = new Throw();
+				emptyValue.CreateThrow(
+					false,
+					"ev",
+					"Empty value. Please try again.",
+					true
+				);
+				program.DoReturn(false);
+			} else {
+				majinCodeSwitcher.CodeSwitch(secretCode);
+			}
 		}
 	}
 }
