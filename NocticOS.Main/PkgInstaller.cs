36359490454a4b018adee916ff51443f0f8298dd
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-28 10:37:02 +0800

    Update PkgInstaller.cs
    Major updates.. :smirk:

--- NocticOS.Main/PkgInstaller.cs
+++ NocticOS.Main/PkgInstaller.cs
@@ -18,11 +18,10 @@ namespace NocticOS.Main
 			Program program = new Program();
 			string? pkgUrl = "";
 			string? path = "";
+
 			Console.Write("\nInput the package you want to install: ");
 			pkgUrl = Console.ReadLine();
-			Console.Write("\nInput the path for the repository to be cloned to: ");
-			path = Console.ReadLine();
-			if (String.IsNullOrEmpty(pkgUrl) || String.IsNullOrEmpty(path)) {
+			if (String.IsNullOrEmpty(pkgUrl)) {
 				Throw nullUrl = new Throw();
 				nullUrl.CreateThrow(
 					false,
@@ -30,12 +29,34 @@ namespace NocticOS.Main
 					"Null / Empty URL, please input the URL again, later.",
 					true
 				);
+			}
+
+			Console.Write("\nInput the path for the repository to be cloned to: ");
+			path = Console.ReadLine();
+			if (String.IsNullOrEmpty(path)) {
+				Throw nullPath = new Throw();
+				nullPath.CreateThrow(
+					false,
+					"np",
+					"Null / Empty Path, please input the path again, later.",
+					true
+				);
+			}
+
+			if (!String.IsNullOrEmpty(pkgUrl) || !String.IsNullOrEmpty(path)) {
+				Repository.Clone(pkgUrl, path);
+				Console.Write("\nSuccessfully installed package.\n");
 				program.DoReturn(false);
 			} else {
-				Repository.Clone(pkgUrl, path);
+				Throw nullUrlAndPath = new Throw();
+				nullUrlAndPath.CreateThrow(
+					false,
+					"nup",
+					"Null / Empty URL / Path, please input the Path and URL again, later.",
+					true
+				);
+				program.DoReturn(false);
 			}
-			Console.Write("\nSuccessfully installed package.\n");
-			program.DoReturn(false);
 		}
 	}
 }
