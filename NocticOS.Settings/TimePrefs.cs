74892e8d2c1b3ee0c7aa3508e675be29222d6ebc
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-31 00:05:21 +0800

    Create TimePrefs.cs
    Decided to add this back, but I'm taking a different approach this time ;P

---
+++ NocticOS.Settings/TimePrefs.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NocticOS.Settings
+{
+	public class TimePrefs
+	{
+		static string currentBorder = "";
+		public string timeBorder = currentBorder;
+		static string currentSep = "";
+		public string sepLine = currentSep;
+		string sampleTime = "23:59:59";
+	}
+}
